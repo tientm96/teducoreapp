6be11369c3cb968a7ad8d7d1ac3e514c20956963
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-06-24 00:54:27 +0700

    lesson13-Seeding sample database

---
+++ TeduCoreApp.Utilities/Constants/CommonConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduCoreApp.Utilities.Constants
+{
+    public class CommonConstants //hằng
+    {
+        public const string DefaultFooterId = "DefaultFooterId";
+    }
+}
--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -11,9 +11,17 @@ using Microsoft.Extensions.DependencyInjection;
 using TeduCoreApp.Data;
 using TeduCoreApp.Models;
 using TeduCoreApp.Services;
+using TeduCoreApp.Data.EF;
+using TeduCoreApp.Data.Entities;
 
 namespace TeduCoreApp
 {
+    /*Cấu hình file statup, thay thế ApplicationDbContext mặc định thành AppDbContext của ta.
+        Mục đích cấu hình để gen ra db đúng với custom của ta.
+    
+        -Sau khi cấu hình xong thì ta xóa folder Data (trong pr MVC) đc gen mặc định từ trước, với
+        ApplicationDbContext
+     */
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,22 +34,48 @@ namespace TeduCoreApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); 
-                                        //"DefaultConnection" lấy trong file appsetting.json
+            //thay vì dùng ApplicationDbContext tạo sẵn, ta dùng AppDbContext tự custom
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                o => o.MigrationsAssembly("TeduCoreApp.Data.EF")));
+            //"DefaultConnection" lấy trong file appsetting.json, đã đc sửa lại.
+            //add object o.MigrationsAssembly để tự động DbContext trong Assembly đấy.
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+
+
+            //cấu hình lại Identity
+            services.AddIdentity<AppUser, AppRole>() //thay vì <ApplicationUser, IdentityRole>
+                .AddEntityFrameworkStores<AppDbContext>() //thay vì ApplicationDbContext
                 .AddDefaultTokenProviders();
+
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
+
+            //Nếu muốn tạo đc dữ liệu User và Role mẫu ở DbInitializer thì phải cấu hình ở đây.
+            //Dùng AddScoped để giới hạn 1 request
+            services.AddScoped<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddScoped<RoleManager<AppRole>, RoleManager<AppRole>>();
+
+
+
+
+            /*Class DbInitializer dùng để khởi tạo một số dlieu để làm việc khi khởi tạo db.
+             * Chúng ta cấu hình class này tại đây.
+             *
+             *  Ta sẽ gọi nó (DbInitializer) dưới cùng của method Configure() ở dưới, bằng
+             *  cách đưa nó vào tham số của Configure()
+             */
+            //Add DbInitializer services
+            services.AddTransient<DbInitializer>();
+
+
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -56,7 +90,7 @@ namespace TeduCoreApp
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
+            app.UseAuthentication();    //bật Authen để sử dụng Identity
 
             app.UseMvc(routes =>
             {
@@ -64,6 +98,11 @@ namespace TeduCoreApp
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //gọi đến hàm Seed() để tạo dữ liệu
+            dbInitializer.Seed().Wait();    //.Wait() gọi theo bất đồng bộ, nghĩa là đợi
+                           //vì bên method dbInitializer.Seed() cấu hình theo bất đồng bộ.
+                           //   Nghĩa là ĐỢI cho Task complete mới thực thi.
         }
     }
 }
